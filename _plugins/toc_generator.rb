aaed95ea258ab90020529b3d6593013c1f9bf586
Garib2 <118709436+Garib2@users.noreply.github.com>
2024-08-07 21:03:40 +0900

    Update toc_generator.rb

--- _plugins/toc_generator.rb
+++ _plugins/toc_generator.rb
@@ -1,11 +1,13 @@
 Jekyll::Hooks.register :pages, :pre_render do |page|
-  toc = []
-  page.content.scan(/<h(\d) id="(.*?)">(.*?)<\/h\1>/).each do |level, id, text|
-    toc << {
-      'level' => level.to_i,
-      'id' => id,
-      'text' => text
-    }
+  if page.extname == '.md'
+    toc = []
+    page.content.scan(/<h(\d) id="(.*?)">(.*?)<\/h\1>/).each do |level, id, text|
+      toc << {
+        'level' => level.to_i,
+        'id' => id,
+        'text' => text
+      }
+    end
+    page.data['toc'] = toc
   end
-  page.data['toc'] = toc
 end
